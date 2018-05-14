8eca65b9b4932cf648794506c2a76863919da963
way2enjoy <arjun@way2enjoy.com>
2018-05-14 12:38:05 +0530

    practical guide to link images through database
    we have given practical example on how to connect through database. if any doubt, feel free to chat 24 hours

--- image-url.php
+++ image-url.php
@@ -5,10 +5,30 @@ error_reporting(0);
 
 require_once("Way2enjoyweb.php");
 
+
+
+// database connection starts here   
+date_default_timezone_set("Asia/Kolkata"); 
+include('/home/something/public_html/somefolder/connecti.php');
+mysqli_set_charset($con, 'utf8mb4');
+$table_to_optimize=array("rice","oils","somemore"); 
+$random_table = $table_to_optimize[array_rand($table_to_optimize, 1)];
+$re7 = mysqli_query($con,"select id,status,prod_img from $random_table where status='0' order by id desc limit 1");
+$row2 = mysqli_fetch_assoc($re7);
+$status=$row2['status'];
+$iddd=$row2['id'];
+$imglink=$row2['prod_img'];
+$imglink_absolute='http://www.sitename.com/images/'.$row2['prod_img'];
+$image_path='/home/something/public_html/somefolder/'.$imglink;
+// database connection ends here
+
+
+
+
 $Way2web = new Way2enjoyweb("your@email.com", "websitename");
 
 $params = array(
-    "url" => "http://i2.wp.com/images.indianexpress.com/2017/06/india-pakistan-759.jpg",
+    "url" => $imglink_absolute,
     "wait" => true,
 //varies from 32-320//    "mp3_bit" => "96",
 //varies from 1-100//    "quality" => "95",
@@ -16,6 +36,7 @@ $params = array(
 //        "width" => 100,
 //        "height" => 75,
 //        "strategy" => "crop"
+	 ),
 );
 
 $data = $Way2web->url($params);
@@ -24,8 +45,11 @@ $data = $Way2web->url($params);
 
 if (!empty($data["success"])) {
 
+	copy($data["compressed_url"], $image_path);
+	
+	mysqli_query($con,"update $random_table set status='1' where id='$iddd' ");	
+	
 	// optimization succeeded
-//	echo "Success. Optimized image URL: " . $data["kraked_url"];
 	echo "Success. Optimized image URL: " . $data["compressed_url"];
 
 } elseif (isset($data["message"])) {
